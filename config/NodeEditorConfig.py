56b73af992da18df8d94e7a690c23b036126a7a7
lilinfang <lilinfangrelax@gmail.com>
2025-02-19 00:59:39 +0800

    1. Add NodeSketchpad, and can drag the pad 2. Need to compate comments...

---
+++ config/NodeEditorConfig.py
@@ -0,0 +1,13 @@
+class NodeEditorConfig:
+    scene_width = 32000
+    scene_height = 32000
+
+    scene_background_color = "#212121"
+
+    scene_grid_normal_line_color = "#313131"
+    scene_grid_dark_line_color = "#131313"
+    scene_grid_normal_line_width = 1.0
+    scene_grid_dark_line_width = 1.0
+
+    scene_grid_size = 20
+    scene_grid_chunk = 10
--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 from PySide6 import QtCore, QtGui, QtWidgets
 from thread.MqttClientThread import MqttClientThread
+from view.NodeSketchpad import *
+
 
 class TestToys(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.setup_node_sketchpad()
+
+    def setup_node_sketchpad(self):
         self.setWindowTitle("TestToys")
+        self.setGeometry(100, 100, 1000, 600)
         self.setWindowIcon(QtGui.QIcon("public/icon.png"))
-        self.resize(400, 400)
+
+        self.scene = NodeSketchpadScene()
+        self.view = NodeSketchpadView(self.scene, self)
+
+        self.layout = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.LeftToRight, self)
+        self.layout.addWidget(self.view)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+
+        # self.button_a = QtWidgets.QPushButton("A")
+        # self.button_b = QtWidgets.QPushButton("B")
+        # self.button_a.clicked.connect(self.click_a)
+        # self.layout.addWidget(self.button_a)
+        # self.layout.addWidget(self.button_b)
+        # self.client_a_thread = MqttClientThread(self, "button_a")
+        # self.client_b_thread = MqttClientThread(self, "button_b")
+        # self.client_a_thread.client_started.connect(self.client_started)
+        # self.client_a_thread.client_stopped.connect(self.client_stopped)
+        # self.client_b_thread.client_started.connect(self.client_started)
+        # self.client_b_thread.client_stopped.connect(self.client_stopped)
+        # self.client_a_thread.start()
+        # self.client_b_thread.start()
         self.show()
-
-        self.button_a = QtWidgets.QPushButton("A")
-        self.button_b = QtWidgets.QPushButton("B")
-        self.button_a.clicked.connect(self.click_a)
-
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.button_a)
-        self.layout.addWidget(self.button_b)
-
-        self.client_a_thread = MqttClientThread(self, "button_a")
-        self.client_b_thread = MqttClientThread(self, "button_b")
-        self.client_a_thread.client_started.connect(self.client_started)
-        self.client_a_thread.client_stopped.connect(self.client_stopped)
-        self.client_b_thread.client_started.connect(self.client_started)
-        self.client_b_thread.client_stopped.connect(self.client_stopped)
-        self.client_a_thread.start()
-        self.client_b_thread.start()
 
     def client_started(self, message):
         print(f"Client Status: {message}")
