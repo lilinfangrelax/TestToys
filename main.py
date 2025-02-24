b4b485439e2e5b8d29add80693e2ec0c73492235
lilinfang <lilinfangrelax@gmail.com>
2025-02-24 22:53:23 +0800

    1. Use QWebEngineView to show HTML content 2. Use QTabWidget to manager sketchpad 3. Save Dock state

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from PySide6 import QtCore, QtGui, QtWidgets
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QDockWidget, QTextEdit, QMainWindow, QListWidget
-from thread.MqttClientThread import MqttClientThread
+import sys
+
+from PySide6 import QtGui, QtWidgets, QtCore
+from PySide6.QtCore import QSettings
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWidgets import QDockWidget, QMainWindow, QListWidget, QTabWidget, QTextEdit, QFrame
 from view.NodeSketchpad import *
 
 
@@ -10,21 +12,26 @@ class TestToys(QMainWindow):
         super().__init__()
         self.setup_node_sketchpad()
 
+    def closeEvent(self, event):
+        settings = QSettings("TestToys", "TestToys")
+        settings.setValue('windowState', self.saveState())
+
     def setup_node_sketchpad(self):
         self.setWindowTitle("TestToys")
         self.setGeometry(100, 100, 1000, 600)
         self.setWindowIcon(QtGui.QIcon("public/icon.png"))
 
+
         self.scene = NodeSketchpadScene()
         self.view = NodeSketchpadView(self.scene, self)
-
-        self.setCentralWidget(self.view)
+        self.center_tabs = QTabWidget()
+        self.center_tabs.addTab(self.view, "Default")
+        self.setCentralWidget(self.center_tabs)
 
         dock1 = QDockWidget("Browser", self)
         dock1_widget = QListWidget()
         dock1.setWidget(dock1_widget)
         self.addDockWidget(Qt.LeftDockWidgetArea, dock1)
-
 
         dock2 = QDockWidget("Properties", self)
         dock2_widget = QListWidget()
@@ -32,16 +39,22 @@ class TestToys(QMainWindow):
         self.addDockWidget(Qt.RightDockWidgetArea, dock2)
 
         dock3 = QDockWidget("Logger", self)
-        dock3_widget = QListWidget()
+        dock3_widget = QTextEdit()
+        dock3_widget.setReadOnly(True)
+        dock3_widget.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        # Solve problem: there is a blue line in the bottom of QTextEdit
+        # https://stackoverflow.com/questions/16436058/how-to-make-qtextedit-with-no-visible-border
+        dock3_widget.setFrameStyle(QFrame.NoFrame)
         dock3.setWidget(dock3_widget)
+        dock3.setMinimumHeight(150)
         self.addDockWidget(Qt.BottomDockWidgetArea, dock3)
 
-        self.splitDockWidget(dock2, dock3, Qt.Vertical)
-
-        dock4 = QDockWidget("Nodes", self)
-        dock4_widget = QListWidget()
-        dock4.setWidget(dock4_widget)
-        self.addDockWidget(Qt.BottomDockWidgetArea, dock4)
+        dock4 = QDockWidget("Result", self)
+        web_view = QWebEngineView()
+        web_view.setHtml('<a href="https://data.typeracer.com/pit/profile?user=hk_l&ref=badge" target="_top"><img src="https://data.typeracer.com/misc/badge?user=hk_l" border="0" alt="TypeRacer.com scorecard for user hk_l"/></a>')
+        dock4.setWidget(web_view)
+        dock4.setEnabled(False)
+        self.addDockWidget(Qt.RightDockWidgetArea, dock4)
 
         # self.button_a = QtWidgets.QPushButton("A")
         # self.button_b = QtWidgets.QPushButton("B")
@@ -69,6 +82,11 @@ class TestToys(QMainWindow):
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
+    app = QtWidgets.QApplication(sys.argv)
     test_toys = TestToys()
+    # the companyName and appName is necessary to save DockWidget state, I don't know why.
+    settings = QSettings("TestToys", "TestToys")
+    state = settings.value('windowState')
+    if state != None:
+        test_toys.restoreState(state)
     app.exec()
